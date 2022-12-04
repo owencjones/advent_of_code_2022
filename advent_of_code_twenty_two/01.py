94f03280a1e7f108c8ff4da986fc7cae513a4f2b
Owen <github@owencjones.co.uk>
2022-12-04 12:12:58 +0000

    Changes to 01.py to add typing and function for testing.

--- advent_of_code_twenty_two/01.py
+++ advent_of_code_twenty_two/01.py
@@ -1,14 +1,22 @@
 from util.read_data import read_data
+from typing import Tuple, List
 
 
 input = read_data("01/input.txt")
 
-elves = sorted(
+elves: List[int] = sorted(
     [
         sum(elf)
         for elf in [list(map(int, elf)) for elf in [elf.split('\n') for elf in input.split("\n\n")]]
     ]
 )
 
+def output(elves) -> Tuple[int, int]:
+    return (
+        elves[-1],
+        sum(elves[-3:])
+    )
 
-print(f"The max elf is {elves[-1]}, and the top three add up to {sum(elves[-3:])}")
+if __name__ == "__main__":
+    output_answers = output(elves)
+    print(f"The max elf is {output_answers[0]}, and the top three add up to {output_answers[1]}")
