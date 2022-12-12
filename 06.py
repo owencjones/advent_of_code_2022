bf1ee5c7834c010a6f8e7293e00d08b06c25db13
Owen <github@owencjones.co.uk>
2022-12-12 21:30:48 +0000

    Part two 06

--- 06.py
+++ 06.py
@@ -2,27 +2,28 @@ from pathlib import Path
 
 data_file = Path('data') / '06' / 'input.txt'
 
+def find_stop_bitof_length(datafile: Path, length = 4) -> int:
+    input_registers = [None] * length
 
-input_registers = [None, None, None, None]
+    with data_file.open() as file:
+        i = 0
+        while True:
+            char = file.read(1)
 
-with data_file.open() as file:
-    i = 0
-    while True:
-        char = file.read(1)
+            i += 1
 
-        i += 1
+            input_registers.pop(0)
+            input_registers.append(char)
 
-        input_registers.pop(0)
-        input_registers.append(char)
+            if any([i is None for i in input_registers]):
+                continue
 
-        if any([i is None for i in input_registers]):
-            continue
+            if len(input_registers) == len(set(input_registers)):
+                return i
+                break
+        else:
+            raise Exception("Reached end without finding code")
 
-        if len(input_registers) == len(set(input_registers)):
-            print(f"Stopped at file position {i}, with code {''.join(input_registers)}")
-            break
-    else:
-        print("Reached end without finding code")
-
-print('Done')
+print("Part 1", find_stop_bitof_length(data_file))
+print("Part 2", find_stop_bitof_length(data_file, 14))
 
